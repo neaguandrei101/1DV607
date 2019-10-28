0e62487c5a516d0205ebf64fac3dbf9b6dddef88
Andrei Neagu <neaguandrei47@gmail.com>
2019-10-28 01:52:26 +0100

    soft17 factory fix

--- Workshop 3/BlackJack/model/rules/RulesFactory.java
+++ Workshop 3/BlackJack/model/rules/RulesFactory.java
@@ -2,11 +2,14 @@ package BlackJack.model.rules;
 
 public class RulesFactory {
 
-  public IHitStrategy GetHitRule() {
+    public IHitStrategy GetHitRule() {
     return new BasicHitStrategy();
   }
+    public IHitStrategy GetSoft17HitStrategy() {
+        return new Soft17HitStrategy();
+    }
 
-  public INewGameStrategy GetNewGameRule() {
+    public INewGameStrategy GetNewGameRule() {
     return new AmericanNewGameStrategy();
   }
 }
