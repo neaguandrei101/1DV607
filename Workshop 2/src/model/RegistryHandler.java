fac7d70d08042fa3f20df6374bb2d3c4437b2244
Andrei Neagu <neaguandrei47@gmail.com>
2019-12-29 05:18:37 +0100

    Changes added

--- Workshop 2/src/model/RegistryHandler.java
+++ Workshop 2/src/model/RegistryHandler.java
@@ -6,12 +6,12 @@ import java.io.IOException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class RegistryHandler {
-	public static BoatClub getBoatClubFromJsonFile(String relativePath)  throws IOException{
+    public static BoatClub getBoatClubFromJsonFile(String relativePath) throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
         return objectMapper.readValue(new File(relativePath), BoatClub.class);
     }
 
-	public static void saveBoatClubToJsonFile(String relativePath, BoatClub boatClub) throws IOException{
+    public static void saveBoatClubToJsonFile(String relativePath, BoatClub boatClub) throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.writeValue(new File(relativePath), boatClub);
     }
